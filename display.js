41be5e863b32e54d8f21c196473ef963929c2b16
fantasia <fantasiacjh@gmail.com>
2019-12-06 03:14:57 +0900

    add option

--- display.js
+++ display.js
@@ -1,180 +1,177 @@
-      var subject = null
-      var verb = null
-      var tense = null
+var subject = null
+var verb = null
+var tense = null
 
+var yPos = 125;
+var circleWidth = 70;
+var lineLength = 70;
+var strokeWidth = 6;
 
-       var yPos = 125;
-       var circleWidth = 70;
-       var lineLength = 70;
-       var strokeWidth = 6;
+function quiz() {
+    subject = pickSubject();
+    verb = pickVerb()
+    tense = pickTense();
 
-
-      function quiz() {
-
-        subject = pickSubject();
-        verb = pickVerb()
-        tense = pickTense();
-
-        console.log(subject+
+    console.log(subject+
         "\n" + verb +
         "\n" + tense +
         "\n" + getResult(subject,verb,tense))
         drawTense(tense.tense,tense.progressive, tense.perfect);
         drawQuizText(subject, verb);
-       }
+}
 
-       function answer() {
-           drawResultText(subject,verb,tense);
-       }
+function answer() {
+   drawResultText(subject,verb,tense);
+}
 
-	   var nextCount = 0
-	   function next() {
-			if( nextCount++ %2 == 0 ) {
-				quiz()
-			} else {
-				answer()
-			}
-	   }
+var nextCount = 0
+function next() {
+    if( nextCount++ %2 == 0 ) {
+        quiz()
+    } else {
+        answer()
+    }
+}
 
-       function drawQuizText(subject, verb) {
-           var canvas = document.getElementById('text-layer');
-            if (!canvas.getContext){
-                return;
-            }
+function drawQuizText(subject, verb) {
+   var canvas = document.getElementById('text-layer');
+    if (!canvas.getContext){
+        return;
+    }
 
-            var ctx = canvas.getContext('2d');
-            ctx.font = '36px serif';
-            ctx.strokeStyle = 'black';
-            ctx.fillStyle = 'black';
-            ctx.clearRect(0, 0, 480,320);
+    var ctx = canvas.getContext('2d');
+    ctx.font = '36px serif';
+    ctx.strokeStyle = 'black';
+    ctx.fillStyle = 'black';
+    ctx.clearRect(0, 0, 480,320);
 
-            ctx.fillText(subject.name, 70, 50);
+    ctx.fillText(subject.name, 70, 50);
 
-            ctx.fillText(verb.base, 270, 50);
+    ctx.fillText(verb.base, 270, 50);
 
-       }
+}
 
-       function drawResultText(subject, verb, tense) {
-           var canvas = document.getElementById('text-layer');
-            if (!canvas.getContext){
-                return;
-            }
+function drawResultText(subject, verb, tense) {
+   var canvas = document.getElementById('text-layer');
+    if (!canvas.getContext){
+        return;
+    }
 
-            var ctx = canvas.getContext('2d');
-            ctx.font = '36px serif';
-            ctx.strokeStyle = 'black';
-            ctx.fillStyle = 'black';
-
-
-            ctx.fillText(getResult(subject,verb,tense), 70, 250);
-            ctx.font = '30px serif';
-            ctx.fillText(verb.kor, 150, 290);
-
-       }
+    var ctx = canvas.getContext('2d');
+    ctx.font = '36px serif';
+    ctx.strokeStyle = 'black';
+    ctx.fillStyle = 'black';
 
 
-      function drawBackground(){
-        var canvas = document.getElementById('background-layer');
-        if (!canvas.getContext){
-            return;
-        }
-        var presentPosX = (480 - circleWidth) / 2
-        var pastPosX = presentPosX - lineLength - circleWidth
-        var futurePosX = presentPosX + circleWidth + lineLength
+    ctx.fillText(getResult(subject,verb,tense), 70, 250);
+    ctx.font = '30px serif';
+    ctx.fillText(verb.kor, 150, 290);
 
-        var ctx = canvas.getContext('2d');
-        ctx.strokeStyle = 'skyBlue';
-        ctx.lineWidth = strokeWidth;
+}
 
-        drawCircle(ctx,pastPosX, yPos, circleWidth);
-        drawCircle(ctx,presentPosX, yPos, circleWidth);
-        drawCircle(ctx,futurePosX, yPos, circleWidth);
 
-        drawHorizontalLine(ctx, pastPosX - lineLength, yPos + (circleWidth/2), lineLength)
-        drawHorizontalLine(ctx, presentPosX - lineLength, yPos + (circleWidth/2), lineLength)
-        drawHorizontalLine(ctx, futurePosX - lineLength, yPos + (circleWidth/2), lineLength)
-        drawHorizontalLine(ctx, futurePosX + circleWidth, yPos + (circleWidth/2), lineLength)
+function drawBackground(){
+    var canvas = document.getElementById('background-layer');
+    if (!canvas.getContext){
+        return;
+    }
+    var presentPosX = (480 - circleWidth) / 2
+    var pastPosX = presentPosX - lineLength - circleWidth
+    var futurePosX = presentPosX + circleWidth + lineLength
 
-      }
-      function drawPast() {
-        drawTense(TenseEnum.past, document.getElementById("progressive").checked, document.getElementById("perfect").checked)
-      }
+    var ctx = canvas.getContext('2d');
+    ctx.strokeStyle = 'skyBlue';
+    ctx.lineWidth = strokeWidth;
 
-      function drawPresent() {
-        drawTense(TenseEnum.present, document.getElementById("progressive").checked, document.getElementById("perfect").checked)
-      }
+    drawCircle(ctx,pastPosX, yPos, circleWidth);
+    drawCircle(ctx,presentPosX, yPos, circleWidth);
+    drawCircle(ctx,futurePosX, yPos, circleWidth);
 
-      function drawFuture() {
-        drawTense(TenseEnum.future, document.getElementById("progressive").checked, document.getElementById("perfect").checked)
-      }
+    drawHorizontalLine(ctx, pastPosX - lineLength, yPos + (circleWidth/2), lineLength)
+    drawHorizontalLine(ctx, presentPosX - lineLength, yPos + (circleWidth/2), lineLength)
+    drawHorizontalLine(ctx, futurePosX - lineLength, yPos + (circleWidth/2), lineLength)
+    drawHorizontalLine(ctx, futurePosX + circleWidth, yPos + (circleWidth/2), lineLength)
+}
 
-      function drawTense(tense, progressive, perfect) {
-         var canvas = document.getElementById('ui-layer');
-        if (!canvas.getContext){
-            return;
-        }
-        var presentPosX = (480 - circleWidth) / 2
-        var pastPosX = presentPosX - lineLength - circleWidth
-        var futurePosX = presentPosX + circleWidth + lineLength
+function drawPast() {
+    drawTense(TenseEnum.past, document.getElementById("progressive").checked, document.getElementById("perfect").checked)
+}
 
-        var ctx = canvas.getContext('2d');
-        ctx.strokeStyle = 'blue';
-        ctx.fillStyle = 'blue';
-        ctx.lineWidth = strokeWidth;
+function drawPresent() {
+    drawTense(TenseEnum.present, document.getElementById("progressive").checked, document.getElementById("perfect").checked)
+}
 
-        ctx.clearRect(0, 0, 480,320);
+function drawFuture() {
+    drawTense(TenseEnum.future, document.getElementById("progressive").checked, document.getElementById("perfect").checked)
+}
 
-        //draw tense circle
-        var xPos = presentPosX;
-        switch(tense) {
-            case TenseEnum.past:
+function drawTense(tense, progressive, perfect) {
+    var canvas = document.getElementById('ui-layer');
+    if (!canvas.getContext){
+        return;
+    }
+    var presentPosX = (480 - circleWidth) / 2
+    var pastPosX = presentPosX - lineLength - circleWidth
+    var futurePosX = presentPosX + circleWidth + lineLength
+
+    var ctx = canvas.getContext('2d');
+    ctx.strokeStyle = 'blue';
+    ctx.fillStyle = 'blue';
+    ctx.lineWidth = strokeWidth;
+
+    ctx.clearRect(0, 0, 480,320);
+
+    //draw tense circle
+    var xPos = presentPosX;
+    switch(tense) {
+        case TenseEnum.past:
             xPos = pastPosX;
             break;
-            case TenseEnum.present:
+        case TenseEnum.present:
             xPos = presentPosX;
             break;
-            case TenseEnum.future:
+        case TenseEnum.future:
             xPos = futurePosX;
             break;
-        }
-        drawCircle(ctx,xPos, yPos, circleWidth);
+    }
+    drawCircle(ctx,xPos, yPos, circleWidth);
 
-        //draw progressive
-        if(progressive) {
-            fillCircle(ctx,xPos, yPos, circleWidth);
-        }
+    //draw progressive
+    if(progressive) {
+        fillCircle(ctx,xPos, yPos, circleWidth);
+    }
 
-        //draw perfect
-        if(perfect) {
-            drawHorizontalLine(ctx, xPos - lineLength, yPos + (circleWidth/2), lineLength)
-            drawVerticalLine(ctx, xPos, yPos, circleWidth)
-        }
-      }
+    //draw perfect
+    if(perfect) {
+        drawHorizontalLine(ctx, xPos - lineLength, yPos + (circleWidth/2), lineLength)
+        drawVerticalLine(ctx, xPos, yPos, circleWidth)
+    }
+}
 
-      function drawHorizontalLine(ctx, startX, startY, length) {
-        ctx.beginPath();
-        ctx.moveTo(startX,startY);
-        ctx.lineTo(startX + length, startY);
-        ctx.stroke();
-      }
+function drawHorizontalLine(ctx, startX, startY, length) {
+    ctx.beginPath();
+    ctx.moveTo(startX,startY);
+    ctx.lineTo(startX + length, startY);
+    ctx.stroke();
+}
 
-      function drawVerticalLine(ctx, startX, startY, length) {
-        ctx.beginPath();
-        ctx.moveTo(startX,startY);
-        ctx.lineTo(startX , startY + length);
-        ctx.stroke();
-      }
+function drawVerticalLine(ctx, startX, startY, length) {
+    ctx.beginPath();
+    ctx.moveTo(startX,startY);
+    ctx.lineTo(startX , startY + length);
+    ctx.stroke();
+}
 
-      function drawCircle(ctx, x, y, radiusX2) {
-        var radius = radiusX2 / 2;
-        var circle = new Path2D();
-        circle.arc(x + radius, y + radius, radius, 0,  2 * Math.PI);
-        ctx.stroke(circle);
-      }
+function drawCircle(ctx, x, y, radiusX2) {
+    var radius = radiusX2 / 2;
+    var circle = new Path2D();
+    circle.arc(x + radius, y + radius, radius, 0,  2 * Math.PI);
+    ctx.stroke(circle);
+}
 
-      function fillCircle(ctx, x, y, radiusX2) {
-        var radius = radiusX2 / 2;
-        var circle = new Path2D();
-        circle.arc(x + radius, y + radius, radius, 0,  2 * Math.PI);
-        ctx.fill(circle);
-      }
+function fillCircle(ctx, x, y, radiusX2) {
+    var radius = radiusX2 / 2;
+    var circle = new Path2D();
+    circle.arc(x + radius, y + radius, radius, 0,  2 * Math.PI);
+    ctx.fill(circle);
+}
--- func.js
+++ func.js
@@ -1,80 +1,90 @@
-
-const TenseEnum = Object.freeze({"present":1, "past":2, "future":3})
-const PersonEnum = Object.freeze({"first":1, "second":2, "third":3})
-const PluralEnum = Object.freeze({"singular":1, "plural":2})
+const TenseEnum = Object.freeze({
+    "present": 1,
+    "past": 2,
+    "future": 3
+})
+const PersonEnum = Object.freeze({
+    "first": 1,
+    "second": 2,
+    "third": 3
+})
+const PluralEnum = Object.freeze({
+    "singular": 1,
+    "plural": 2
+})
 
 function tenseToString(tense) {
-  var ret = "";
-  switch(tense) {
-    case TenseEnum.present:  
-      ret = "present";
-      break;
-    case TenseEnum.past:  
-      ret = "past";
-      break;
-    case TenseEnum.present:  
-      ret = "future";
-      break;
-  }
-  return ret;
+    var ret = "";
+    switch (tense) {
+        case TenseEnum.present:
+            ret = "present";
+            break;
+        case TenseEnum.past:
+            ret = "past";
+            break;
+        case TenseEnum.present:
+            ret = "future";
+            break;
+    }
+    return ret;
 }
 
 function personToString(person) {
-  var ret = "";
-  switch(person) {
-    case PersonEnum.first:  
-      ret = "first";
-      break;
-    case PersonEnum.second:  
-      ret = "second";
-      break;
-    case PersonEnum.third:  
-      ret = "third";
-      break;
-  }
-  return ret;
+    var ret = "";
+    switch (person) {
+        case PersonEnum.first:
+            ret = "first";
+            break;
+        case PersonEnum.second:
+            ret = "second";
+            break;
+        case PersonEnum.third:
+            ret = "third";
+            break;
+    }
+    return ret;
 }
 
 function pluralToString(plural) {
-  var ret = "";
-  switch(plural) {
-    case PluralEnum.singular:  
-      ret = "singular";
-      break;
-    case PluralEnum.plural:  
-      ret = "plural";
-      break;
-  }
-  return ret;
+    var ret = "";
+    switch (plural) {
+        case PluralEnum.singular:
+            ret = "singular";
+            break;
+        case PluralEnum.plural:
+            ret = "plural";
+            break;
+    }
+    return ret;
 }
 
 function tenseToString(tense) {
-  var ret = "";
-  switch(tense) {
-    case TenseEnum.present:  
-      ret = "present";
-      break;
-    case TenseEnum.past:  
-      ret = "past";
-      break;
-    case TenseEnum.present:  
-      ret = "future";
-      break;
-  }
-  return ret;
+    var ret = "";
+    switch (tense) {
+        case TenseEnum.present:
+            ret = "present";
+            break;
+        case TenseEnum.past:
+            ret = "past";
+            break;
+        case TenseEnum.present:
+            ret = "future";
+            break;
+    }
+    return ret;
 }
 
-class Subject{
-  constructor(name, person, plural) {
-      this.name = name;
-      this.person = person;
-      this.plural = plural;
-  }
-  toString(){
-    return "name:" + this.name + 
-    "\nperson:" + personToString(this.person) + 
-    "\nplural:" + pluralToString(this.plural);
-  }
+class Subject {
+    constructor(name, person, plural) {
+        this.name = name;
+        this.person = person;
+        this.plural = plural;
+    }
+    toString() {
+        return "name:" + this.name +
+            "\nperson:" + personToString(this.person) +
+            "\nplural:" + pluralToString(this.plural);
+    }
 }
 
 const subjects = [
@@ -89,18 +99,18 @@ const subjects = [
 ];
 
 
-class Tense{
-  constructor(tense, progressive, perfect) {
-      this.tense = tense;
-      this.progressive = progressive;
-      this.perfect = perfect;
-  }
+class Tense {
+    constructor(tense, progressive, perfect) {
+        this.tense = tense;
+        this.progressive = progressive;
+        this.perfect = perfect;
+    }
 
-  toString(){
-    return "tense:" + tenseToString(this.tense) + 
-    "\nprogressive:" + this.progressive + 
-    "\nperfect:" + this.perfect;
-  }
+    toString() {
+        return "tense:" + tenseToString(this.tense) +
+            "\nprogressive:" + this.progressive +
+            "\nperfect:" + this.perfect;
+    }
 
 }
 
@@ -122,40 +132,48 @@ const tenses = [
 ];
 
 class Verb {
-  constructor(base, past, perfect, progressive, kor, third) {
-    this.base = base;
-    this.past = past;
-    this.perfect = perfect;
-    this.progressive = progressive;
-    this.kor = kor;
-    this.third = third;
-  }
+    constructor(base, past, perfect, progressive, kor, third) {
+        this.base = base;
+        this.past = past;
+        this.perfect = perfect;
+        this.progressive = progressive;
+        this.kor = kor;
+        this.third = third;
+    }
 
-  toString(){
-    return "base:" + this.base + 
-    "\npast:" + this.past + 
-    "\nperfect:" + this.perfect +
-    "\nprogressive:" + this.progressive +
-    "\nkor:" + this.kor +
-    "\nthird:" + this.third;
-  }
-}
-
-const verbs = []
-
-function loadVerbs() {
-    for(var i = 0; i < data.length; i++) {
-        var item = data[i]
-        verbs[i] = new Verb(item[0], item[1], item[2], item[3], item[4], item[5])
+    toString() {
+        return "base:" + this.base +
+            "\npast:" + this.past +
+            "\nperfect:" + this.perfect +
+            "\nprogressive:" + this.progressive +
+            "\nkor:" + this.kor +
+            "\nthird:" + this.third;
     }
 }
 
-loadVerbs()
+const verbs = [];
+
+(function () {
+    for (var i = 0; i < data.length; i++) {
+        var item = data[i];
+        verbs[i] = new Verb(item[0], item[1], item[2], item[3], item[4], item[5]);
+    }
+})()
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+}
+
+function shuffle(array) {
+    var j, x, i;
+    for (i = array.length; i; i -= 1) {
+        j = Math.floor(Math.random() * i);
+        x = array[i - 1];
+        array[i - 1] = array[j];
+        array[j] = x;
+    }
 }
 
 function pickSubject() {
@@ -164,8 +182,20 @@ function pickSubject() {
 }
 
 function pickTense() {
-    var index = getRandomInt(0, tenses.length);
-    return tenses[index];
+    var arTense = []
+    if(cbPresent.checked) { arTense.push(TenseEnum.present) }
+    if(cbPast.checked) { arTense.push(TenseEnum.past) }
+    if(cbFuture.checked) { arTense.push(TenseEnum.future) }
+    if( arTense.length == 0 ) { arTense.push(TenseEnum.present)}
+    shuffle(arTense)
+
+    var progressive = false
+    if( cbProgressive.checked ) { progressive = Math.floor(Math.random() * 2) == 0 }
+
+    var perfect = false
+    if( cbPerfect.checked ) { perfect = Math.floor(Math.random() * 2) == 0 }
+
+    return new Tense(arTense[0], progressive, perfect)
 }
 
 function pickVerb() {
@@ -174,97 +204,153 @@ function pickVerb() {
 }
 
 function getResult(subject, verb, tense) {
-  var name = subject.name;
+    var name = subject.name;
 
-  var verbPrefix = "";
-  var displayVerb = "";
-    switch(tense.tense) {
-      case TenseEnum.present: {
-        if(tense.perfect == true) {
-          if(subject.person == PersonEnum.third && subject.plural == PluralEnum.singular) {
-            verbPrefix += "has ";
-          }else {
-            verbPrefix += "have ";
-          }
+    var verbPrefix = "";
+    var displayVerb = "";
+    switch (tense.tense) {
+        case TenseEnum.present: {
+            if (tense.perfect == true) {
+                if (subject.person == PersonEnum.third && subject.plural == PluralEnum.singular) {
+                    verbPrefix += "has ";
+                } else {
+                    verbPrefix += "have ";
+                }
 
-          if(tense.progressive == true) {
-            verbPrefix += "been ";
-            displayVerb = verb.progressive;
-          }else {
-            displayVerb = verb.perfect;
-          }
+                if (tense.progressive == true) {
+                    verbPrefix += "been ";
+                    displayVerb = verb.progressive;
+                } else {
+                    displayVerb = verb.perfect;
+                }
 
-        }else{
-          if(tense.progressive == true) {
-            if(subject.person == PersonEnum.first && subject.plural == PluralEnum.singular) {
-              verbPrefix += "am ";
-            } else if(subject.person == PersonEnum.third && subject.plural == PluralEnum.singular) {
-              verbPrefix += "is ";
-            }else {
-              verbPrefix += "are ";
-            }
-            displayVerb = verb.progressive;
-          }else {
-            if(subject.person == PersonEnum.third && subject.plural == PluralEnum.singular) {
-              displayVerb = verb.third;
             } else {
-              displayVerb = verb.base;
+                if (tense.progressive == true) {
+                    if (subject.person == PersonEnum.first && subject.plural == PluralEnum.singular) {
+                        verbPrefix += "am ";
+                    } else if (subject.person == PersonEnum.third && subject.plural == PluralEnum.singular) {
+                        verbPrefix += "is ";
+                    } else {
+                        verbPrefix += "are ";
+                    }
+                    displayVerb = verb.progressive;
+                } else {
+                    if (subject.person == PersonEnum.third && subject.plural == PluralEnum.singular) {
+                        displayVerb = verb.third;
+                    } else {
+                        displayVerb = verb.base;
+                    }
+                }
             }
-          }
+            break;
         }
-        break;
-      }
-      case TenseEnum.past: {
-        if(tense.perfect == true) {
-          verbPrefix += "had ";
+        case TenseEnum.past: {
+            if (tense.perfect == true) {
+                verbPrefix += "had ";
+                if (tense.progressive == true) {
+                    verbPrefix += "been ";
+                    displayVerb = verb.progressive;
+                } else {
+                    displayVerb = verb.perfect;
+                }
 
-          if(tense.progressive == true) {
-            verbPrefix += "been ";
-            displayVerb = verb.progressive;
-          }else {
-            displayVerb = verb.perfect;
-          }
-
-        }else {
-          if(tense.progressive == true) {
-            if(subject.person == PersonEnum.first && subject.plural == PluralEnum.singular) {
-              verbPrefix += "was ";
-            } else if(subject.person == PersonEnum.third && subject.plural == PluralEnum.singular) {
-              verbPrefix += "was ";
-            }else {
-              verbPrefix += "were ";
+            } else {
+                if (tense.progressive == true) {
+                    if (subject.person == PersonEnum.first && subject.plural == PluralEnum.singular) {
+                        verbPrefix += "was ";
+                    } else if (subject.person == PersonEnum.third && subject.plural == PluralEnum.singular) {
+                        verbPrefix += "was ";
+                    } else {
+                        verbPrefix += "were ";
+                    }
+                    displayVerb = verb.progressive;
+                } else {
+                    displayVerb = verb.past;
+                }
             }
-            displayVerb = verb.progressive;
-          } else {
-            displayVerb = verb.past;
-          }
+            break;
         }
-        break;
-      }
-      case TenseEnum.future: {
-        verbPrefix += "will ";
-        if(tense.perfect == true) {
-          verbPrefix += "have ";
 
-          if(tense.progressive == true) {
-            verbPrefix += "been ";
-            displayVerb = verb.progressive;
-          } else {
-            displayVerb = verb.perfect;
-          }
-        } else {
+        case TenseEnum.future: {
+            verbPrefix += "will ";
+            if (tense.perfect == true) {
+                verbPrefix += "have ";
+                if (tense.progressive == true) {
+                    verbPrefix += "been ";
+                    displayVerb = verb.progressive;
+                } else {
+                    displayVerb = verb.perfect;
+                }
 
-          if(tense.progressive == true) {
-            verbPrefix += "be ";
-            displayVerb = verb.progressive;
-          } else {
-            displayVerb = verb.base;
-          }
+            } else {
+                if (tense.progressive == true) {
+                    verbPrefix += "be ";
+                    displayVerb = verb.progressive;
+                } else {
+                    displayVerb = verb.base;
+                }
+            }
+            break;
         }
-        break;
-      }
     }
-  
-    return name + " " + verbPrefix + displayVerb
 
+    return name + " " + verbPrefix + displayVerb
+}
+
+function onLoadBody() {
+    drawBackground();
+    loadCheckboxStatus();
+}
+
+window.onload = function() {
+    cbPast = document.getElementById("cbPast");
+    cbPresent = document.getElementById("cbPresent");
+    cbFuture = document.getElementById("cbFuture");
+
+    cbProgressive = document.getElementById("cbProgressive");
+    cbPerfect = document.getElementById("cbPerfect");
+}
+
+function onclickCheckbox(event) {
+    saveCheckboxStatus();
+}
+
+function loadCheckboxStatus() {
+    if( null == window.localStorage.getItem("cbPast") ) {
+        cbPast.checked = true;
+    } else {
+        cbPast.checked = window.localStorage.getItem("cbPast") == "true";
+    }
+
+    if( null == window.localStorage.getItem("cbPresent") ) {
+        cbPresent.checked = true;
+    } else {
+        cbPresent.checked = window.localStorage.getItem("cbPresent") == "true";
+    }
+
+    if( null == window.localStorage.getItem("cbFuture") ) {
+        cbFuture.checked = true;
+    } else {
+        cbFuture.checked = window.localStorage.getItem("cbFuture") == "true";
+    }
+
+    if( null == window.localStorage.getItem("cbProgressive") ) {
+        cbProgressive.checked = true;
+    } else {
+        cbProgressive.checked = window.localStorage.getItem("cbProgressive") == "true";
+    }
+
+    if( null == window.localStorage.getItem("cbPerfect") ) {
+        cbPerfect.checked = true;
+    } else {
+        cbPerfect.checked = window.localStorage.getItem("cbPerfect") == "true";
+    }
+}
+
+function saveCheckboxStatus() {
+    window.localStorage.setItem("cbPast", cbPast.checked);
+    window.localStorage.setItem("cbPresent", cbPresent.checked);
+    window.localStorage.setItem("cbFuture", cbFuture.checked);
+    window.localStorage.setItem("cbProgressive", cbProgressive.checked);
+    window.localStorage.setItem("cbPerfect", cbPerfect.checked);
 }
