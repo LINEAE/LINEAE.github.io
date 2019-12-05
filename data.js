e62418149b10d816d5349b6c949c8d070f7df726
Robert.Choi <donggyuml@linecorp.com>
2019-12-05 12:05:18 +0900

    동사 추가 중

--- data.js
+++ data.js
@@ -2,4 +2,12 @@ var data = [
     ["drive", "drove", "driven", "driving", "운전하다", "drives"],
     ["ride", "rode", "ridden", "riding", "(탈것에)타다", "rides"],
     ["write", "wrote", "written", "writing", "(글씨를)쓰다", "writes"],
+    ["break", "broke", "broken", "breaking", "부수다", "breaks"],
+    ["choose", "chose", "chosen", "choosing", "선택하다", "chooses"],
+    ["forget", "forgot", "forgotten", "forgetting", "잊다", "forgets"],
+    ["freeze", "froze", "frozen", "freezing", "얼다", "freezes"],
+    ["speak", "spoke", "spken", "speaking", "말하다, 연설하다", "speaks"],
+    ["steal", "stole", "stolen", "stealing", "훔치다", "steals"],
+    ["begin", "began", "begun", "beginning", "시작하다", "begins"],
+    ["drink", "drank", "drunk", "drinking", "마시다", "drinks"],
 ]
