02a5a5677cb695bdc0e4be3d2247ad9cc2597f98
juho.cho <juho.cho@linecorp.com>
2019-12-10 09:49:13 +0900

    한->영 변환 추가

--- adjectiveMemorization.js
+++ adjectiveMemorization.js
@@ -14,24 +14,29 @@ function next() {
 
 function quiz() {
     adjective = pickAdjective()
-   
-    drawQuizText(adjective);
+
+    drawCategory(adjective);
+    if( cbKrToEn.checked ) {
+        drawResultText(adjective);
+    } else {
+        drawQuizText(adjective);
+    }
 }
 
 function answer() {
-    drawResultText(adjective)
+    if( cbKrToEn.checked ) {
+        drawQuizText(adjective);
+    } else {
+        drawResultText(adjective);
+    }
 }
-
-function onLoadBody() {
-}
-
 
 function pickAdjective() {
     var index = getRandomInt(0, adjectives.length);
     return adjectives[index];
 }
 
-function drawQuizText(adjective) {
+function drawCategory(adjective) {
    var canvas = document.getElementById('text-layer');
     if (!canvas.getContext){
         return;
@@ -44,6 +49,19 @@ function drawQuizText(adjective) {
     ctx.strokeStyle = 'white';
     ctx.fillStyle = 'white';
     ctx.fillText(adjective.category, 70, 50, 340);
+}
+
+function drawQuizText(adjective) {
+   var canvas = document.getElementById('text-layer');
+    if (!canvas.getContext){
+        return;
+    }
+
+    var ctx = canvas.getContext('2d');
+
+    ctx.font = '36px serif';
+    ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'white';
     ctx.fillText(adjective.eng, 70, 150, 340);
 }
 
@@ -58,4 +76,29 @@ function drawResultText(adjective) {
     ctx.strokeStyle = 'white';
     ctx.fillStyle = '#36A1D5';
     ctx.fillText(adjective.kor, 70, 250 , 340);
+}
+
+
+function onLoadBody() {
+    loadCheckboxStatus();
+}
+
+window.onload = function() {
+    cbKrToEn = document.getElementById("cbKrToEn");
+}
+
+function onclickCheckbox(event) {
+    saveCheckboxStatus();
+}
+
+function loadCheckboxStatus() {
+    if( null == window.localStorage.getItem("cbKrToEn") ) {
+        cbKrToEn.checked = true;
+    } else {
+        cbKrToEn.checked = window.localStorage.getItem("cbKrToEn") == "true";
+    }
+}
+
+function saveCheckboxStatus() {
+    window.localStorage.setItem("cbKrToEn", cbKrToEn.checked);
 }
